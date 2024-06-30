30bef297b4808d9f8691b470171b35c4b99e6f13
Darek Podczasik <boostertech@mail.com>
2024-06-30 14:01:49 +0100

    <BillAmount> created initally

---
+++ src/components/BillAmount/BillAmount.js
@@ -0,0 +1,10 @@
+const BillAmount = () => {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input type="text" value="0"></input>
+    </div>
+  );
+};
+
+export default BillAmount;
