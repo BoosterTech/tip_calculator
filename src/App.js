a3e70c4f32b2b440c3c9b74b37a877e5a149b0ec
Darek Podczasik <boostertech@mail.com>
2024-06-30 14:00:07 +0100

    static components initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import BillAmount from "./components/BillAmount/BillAmount";
+import Output from "./components/Output/Output";
+import Reset from "./components/Reset/Reset";
+import SelectService from "./components/SelectService/SelectService";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
 }
 
 export default App;
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillAmount />
+      <SelectService />
+      <SelectService />
+      <Output />
+      <Reset />
+    </div>
+  );
+}
