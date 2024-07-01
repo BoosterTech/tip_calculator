133e2e1cca66534d3416321096ae3bec62b94ede
Darek Podczasik <boostertech@mail.com>
2024-07-01 20:21:38 +0100

    tip prop received by Output and added for display

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -1,7 +1,9 @@
-const Output = ({ billAmount }) => {
+const Output = ({ billAmount, tip }) => {
   return (
     <div>
-      <span>{`You pay $...($${billAmount}... + $... tip)`} </span>
+      <span>
+        You pay ${billAmount + tip} (${billAmount} + ${tip} tip)
+      </span>
     </div>
   );
 };
