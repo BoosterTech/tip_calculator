2fff09ff1d789ffbf2857523eeaeef4db6dee0dd
Darek Podczasik <boostertech@mail.com>
2024-07-01 20:20:08 +0100

    tipPercentage states created and passed as props to SelectPercentage together with their setting functions. handlereset() function created for resetting all states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 import BillAmount from "./components/BillAmount/BillAmount";
 import Output from "./components/Output/Output";
 import Reset from "./components/Reset/Reset";
-import SelectService from "./components/SelectService/SelectService";
+import SelectPercentage from "./components/SelectPercentage/SelectPercentage";
 
 function App() {
   return (
@@ -13,18 +13,32 @@ function App() {
   );
 }
 
-export default App;
-
 function TipCalculator() {
   const [amount, setAmount] = useState("");
+  const [tipPercetage1, setTipPercentage1] = useState(0);
+  const [tipPercetage2, setTipPercentage2] = useState(0);
+
+  const tip = amount * ((tipPercetage1 + tipPercetage2) / 100);
+
+  function handleReset() {
+    setTipPercentage1(0);
+    setTipPercentage2(0);
+    setAmount("");
+  }
 
   return (
     <div>
       <BillAmount amount={amount} onSetAmount={setAmount} />
-      <SelectService>How did you like</SelectService>
-      <SelectService>How did your friend like the service?</SelectService>
-      <Output billAmount={amount} />
-      <Reset />
+      <SelectPercentage percentage={tipPercetage1} onSelect={setTipPercentage1}>
+        How did you like the service?
+      </SelectPercentage>
+      <SelectPercentage percentage={tipPercetage2} onSelect={setTipPercentage2}>
+        How did your friend like the service?
+      </SelectPercentage>
+      <Output billAmount={amount} tip={tip} />
+      <Reset onReset={handleReset} />
     </div>
   );
 }
+
+export default App;
